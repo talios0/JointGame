ab72b6bf7182b52aae29634351c065c3517bacff
talios0 <20057606+talios0@users.noreply.github.com>
2020-08-20 16:31:10 -0400

    Starting to make transition

--- Assets/_Scripts/Player/Universal/PlayerManager.cs
+++ Assets/_Scripts/Player/Universal/PlayerManager.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class PlayerManager : MonoBehaviour
 {
@@ -17,6 +20,19 @@ public class PlayerManager : MonoBehaviour
     public DroneCamera droneCamera;
     public Camera attachedDroneCamera;
     public DroneMovement droneMovement;
+    public GameObject droneUI;
+
+    [Header("Transition")]
+    public VolumeProfile finalProfile;
+    public VolumeProfile startProfile;
+    public Volume transitionVolume;
+    public Transform transitionTransform;
+    public Camera transitionCamera;
+
+    [Header("Bloom")]
+    public Vector2 bloom;
+
+    private bool transition = false;
 
     private void Start()
     {
@@ -28,8 +44,10 @@ public class PlayerManager : MonoBehaviour
         if (Input.GetButtonDown("Switch")) ShiftPlayerPlayState();
     }
 
-    public void ChangePlayerPlayState(PlayerPlayState state) {
-        switch (state) {
+    public void ChangePlayerPlayState(PlayerPlayState state)
+    {
+        switch (state)
+        {
             case PlayerPlayState.STANDARD:
                 playState = state;
                 TransitionToStandard();
@@ -44,8 +62,10 @@ public class PlayerManager : MonoBehaviour
         }
     }
 
-    public void ShiftPlayerPlayState() {
-        switch (playState) {
+    public void ShiftPlayerPlayState()
+    {
+        switch (playState)
+        {
             case PlayerPlayState.STANDARD:
                 ChangePlayerPlayState(PlayerPlayState.DRONE);
                 break;
@@ -58,31 +78,89 @@ public class PlayerManager : MonoBehaviour
         }
     }
 
-    public PlayerPlayState GetPlayState() {
+    public PlayerPlayState GetPlayState()
+    {
         return playState;
     }
 
-    private void TransitionToStandard() {
+    private void TransitionToStandard()
+    {
+        if (transition) return;
+        transition = true;
+        transitionTransform.position = droneCamera.transform.position;
+        transitionTransform.rotation = droneCamera.transform.rotation;
         // Disable
         droneMovement.DisableMovement();
         attachedDroneCamera.enabled = false;
         droneCamera.enabled = false;
+        droneUI.SetActive(false);
 
-        // Enable
-        standardMovement.EnableMovement();
-        attachedStandardCamera.enabled = true;
-        standardCamera.enabled = true;
+        transitionCamera.enabled = true;
+        StartCoroutine(Transition(droneCamera.transform.position, standardCamera.transform.position, attachedDroneCamera, attachedStandardCamera));
     }
 
-    private void TransitionToDrone() {
+    private void TransitionToDrone()
+    {
+        if (transition) return;
+        transition = true;
+        transitionTransform.position = standardCamera.transform.position;
+        transitionTransform.rotation = standardCamera.transform.rotation;
+        transitionVolume.profile = startProfile;
         // Disable
         standardMovement.DisableMovement();
         attachedStandardCamera.enabled = false;
         standardCamera.enabled = true;
 
-        // Enable
-        droneMovement.EnableMovement();
-        attachedDroneCamera.enabled = true;
-        droneCamera.enabled = true;
+        transitionCamera.enabled = true;
+        StartCoroutine(Transition(standardCamera.transform.position, droneCamera.transform.position, attachedStandardCamera, attachedDroneCamera));
+    }
+
+    private void EndTransition() {
+        switch (playState) {
+            case PlayerPlayState.STANDARD:
+                standardMovement.EnableMovement();
+                attachedStandardCamera.enabled = true;
+                standardCamera.enabled = true;
+                break;
+            case PlayerPlayState.DRONE:
+                droneMovement.EnableMovement();
+                attachedDroneCamera.enabled = true;
+                droneCamera.enabled = true;
+                droneUI.SetActive(true);
+                break;
+            default:
+                Debug.LogWarning("Unknown End Transition State: " + playState);
+                break;
+        }
+
+        transitionCamera.enabled = false;
+        transitionVolume.enabled = false;
+        transition = false;
+    }
+
+    IEnumerator Transition(Vector3 startPos, Vector3 finalPos, Camera startCam, Camera endCam)
+    {
+        transitionVolume.enabled = true;
+        Bloom targetBloom = null;
+        if (!transitionVolume.profile.TryGet(out targetBloom))
+        {
+            Debug.LogError("Unable to retrieve Bloom");
+            yield break;
+        }
+        for (float i = 0; i < Mathf.PI; i += 0.1f)
+        {
+            float weight = Mathf.Cos(i + Mathf.PI) / 2 + 0.5f;
+            transitionTransform.position = new Vector3(Blend(startPos.x, finalPos.x, weight), Blend(startPos.y, finalPos.y, weight), Blend(startPos.z, finalPos.z, weight));
+            targetBloom.intensity.value = Blend(bloom.x, bloom.y, weight); ;
+            transitionCamera.fieldOfView = Blend(startCam.fieldOfView, endCam.fieldOfView, weight);
+            transitionCamera.transform.rotation = Quaternion.Lerp(startCam.transform.rotation, endCam.transform.rotation, i / Mathf.PI);
+            yield return new WaitForFixedUpdate();
+        }
+        EndTransition();
+        yield break;
+    }
+
+    private float Blend(float start, float end, float weight) { 
+        return (end * weight) + (start * (1 - weight));
     }
 }
